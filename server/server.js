d370cc2d354d95b75d34af11ff134be599817b85
Francisco <friverad14@gmail.com>
2023-02-23 16:21:47 -0700

    final commit

--- server/server.js
+++ server/server.js
@@ -12,15 +12,17 @@ app.use(routes)
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   }
   
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client/build/index.html'))
+    })
+
+
   db.once('open', () => {
       app.listen(port, () => {
           console.log(`🌍 Now listening on localhost:${port}`)
       })
   })
   
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  })
