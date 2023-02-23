25d96f99746a2c8bfc438d50f1c066bfb9c0a467
Francisco <friverad14@gmail.com>
2023-02-23 16:28:07 -0700

    more changes

--- server/server.js
+++ server/server.js
@@ -6,17 +6,17 @@ const path = require('path')
 const port = process.env.PORT || 3001;
 const app = express();
 
+app.use(routes)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(routes)
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '..client/build')));
   }
   
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client/build/index.html'))
+      res.sendFile(path.join(__dirname, '..client/build/index.html'))
     })
 
 
