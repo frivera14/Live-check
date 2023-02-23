93cf422e38519c2a63d45155f8f2d96d05b1d785
Francisco <friverad14@gmail.com>
2023-02-23 16:14:23 -0700

    edits in hope it works

--- server/server.js
+++ server/server.js
@@ -15,12 +15,12 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
+  db.once('open', () => {
+      app.listen(port, () => {
+          console.log(`🌍 Now listening on localhost:${port}`)
+      })
+  })
+  
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
-
-db.once('open', () => {
-    app.listen(port, () => {
-        console.log(`🌍 Now listening on localhost:${port}`)
-    })
-})
