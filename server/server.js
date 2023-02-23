0fb124c1dd4710e0fa922b17b07e936ae68315fa
Francisco <friverad14@gmail.com>
2023-02-23 15:30:01 -0700

    changes to server static

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(routes)
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use('/static', express.static(path.join(__dirname, '../client/build')));
   }
   
 app.get('*', (req, res) => {
