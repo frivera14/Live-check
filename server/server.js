dedfa036b226403bc69e4c207a1e407f51c74243
Francisco <friverad14@gmail.com>
2023-02-23 18:25:50 -0700

    final changes

--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ app.use(routes)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  }
-  
+ 
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client/build/index.html'))
     })
